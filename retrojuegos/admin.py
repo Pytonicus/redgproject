5007f7df08497726a29197d67d1c9aba6e26d83f
Guillermo Granados Gómez <guillermogranadosgomez@gmail.com>
2021-05-13 01:08:36 +0200

    añadidos sistemas

--- retrojuegos/admin.py
+++ retrojuegos/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Sistema, Fabricante
 
-# Register your models here.
+admin.site.register(Fabricante)
+admin.site.register(Sistema)
--- retrojuegos/models.py
+++ retrojuegos/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+class Fabricante(models.Model):
+    """ Fabricante de sistemas """
+
+    nombre = models.CharField(max_length=200, verbose_name="Marca")
+    fundacion = models.DateField(verbose_name="Año de Fundación", blank=True)
+    logo = models.ImageField(upload_to='retrogames/logos', verbose_name="Logo", blank=True)
+
+    fecha_creacion = models.DateTimeField(auto_now_add = True, verbose_name="Creado el")
+    fecha_edicion = models.DateField(auto_now = True, verbose_name="Editado el")
+
+    class Meta:
+        verbose_name = "Fabricante"
+        verbose_name_plural = "Fabricantes"
+        ordering = ["nombre"]
+
+    def __str__(self):
+        return self.nombre
+
+class Sistema(models.Model):
+    """ Modelo de Sistemas """
+
+    GENERACION = [
+        ('1', '1ª Generación'),
+        ('2', '2ª Generación'),
+        ('3', '3ª Generación'),
+        ('4', '4ª Generación'),
+        ('5', '5ª Generación'),
+        ('6', '6ª Generación'),
+        ('7', '7ª Generación'),
+        ('8', '8ª Generación')
+    ]
+
+    nombre = models.CharField(max_length=200, verbose_name="Sistema")
+    fabricante = models.ForeignKey(Fabricante, on_delete=models.CASCADE, verbose_name="Fabricante")
+    fecha_lanzamiento = models.DateField(verbose_name="Fecha de lanzamiento", blank=True)
+    generacion = models.CharField(max_length=200, choices=GENERACION, verbose_name="Generación")
+    logo = models.ImageField(upload_to='retrogames/logos', verbose_name="Logo", blank=True)
+    consola = models.ImageField(upload_to='retrogames/consolas', verbose_name="Consola", blank=True)
+    icono = models.ImageField(upload_to='retrogames/iconos', verbose_name="Icono", blank=True)
+
+
+    fecha_creacion = models.DateTimeField(auto_now_add = True, verbose_name="Creado el")
+    fecha_edicion = models.DateField(auto_now = True, verbose_name="Editado el")
+
+    class Meta:
+        verbose_name = "Sistema"
+        verbose_name_plural = "Sistemas"
+        ordering = ["nombre"]
+
+    def __str__(self):
+        return self.nombre
